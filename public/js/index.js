161d998b9e6b758044069eedc75c944d1044c2bb
walidmash <waleedmash2@gmail.com>
2017-08-08 12:02:08 +0300

    the main structure for all team

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+(()=>{
+  dom();
+})()
+
+function dom() {
+
+}
+
+// Generate repors html as string
+function myRepos(obj) {
+  //return string with html needed to display repos
+
+}
+
+//Return object of only the data needed in dom
+function apiReq(userName , callback) {
+//xhr >> backend
+}
---
+++ src/router.js
@@ -0,0 +1,6 @@
+/*
+3 routes to use
+/ route
+/search for searching in github
+/generic for css img js html
+*/
