343d847156d7c19dadd386c92eb076b61c64ac25
samer elaila <samer-elaila@live.com>
2017-08-08 14:22:51 +0300

    Upload src updates

--- src/router.js
+++ src/router.js
@@ -4,3 +4,22 @@
 /search for searching in github
 /generic for css img js html
 */
+const handlers = require("./handlers.js");
+
+function router(req, res){
+  let url = req.url;
+  if(url == "/"){
+    handlers.handleHome(req, res);
+  }
+  else if(url == "/search"){
+    handlers.handleSearch(req, res);
+  }
+  else if(url == '/404'){
+    handlers.handleNotFound(req, res);
+  }
+  else{
+    handlers.handleGeneric(req, res);
+  }
+}
+
+module.exports = router;
