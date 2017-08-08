8148cb3b368b6a165b54a805bdb6efe1284b64a9
walidmash <waleedmash2@gmail.com>
2017-08-08 16:20:57 +0300

    refactor dom and index.html

--- public/js/index.js
+++ public/js/index.js
@@ -3,33 +3,23 @@
 })();
 
 function dom() {
-
-
-
-var searchForm = document.getElementById('searchForm');
-
-    if (searchForm) {
+  var searchForm = document.getElementById('searchForm');
+  if (searchForm) {
     searchForm.addEventListener('submit', function(event){
       event.preventDefault();
-
-
       var userName = event.target.firstElementChild.value;
       apiReq(userName , function(data){
-	  
-      document.querySelector('.avatar img').setAttribute("src", data.avatar_url);
-      document.querySelector('.names h1').textContent = data.name;
-      document.querySelector('.names h6').textContent = data.login;
-      document.querySelector('#company').textContent = data.company;
-      document.querySelector('#location').textContent = data.location;
-      document.querySelector('#followersNumber span').textContent = data.followers;
-      document.querySelector('#followingNumber span').textContent = data.following;
-
-
-
+        document.querySelector('.avatar img').setAttribute("src", data.avatar_url);
+        document.querySelector('.names h1').textContent = data.name;
+        document.querySelector('.names h6').textContent = data.login;
+        document.querySelector('#company').textContent = data.company;
+        document.querySelector('#location').textContent = data.location;
+        document.querySelector('#followersNumber span').textContent = data.followers;
+        document.querySelector('#followingNumber span').textContent = data.following;
+        document.querySelector('.listRepos').innerHTML=myRepos(data.repos);
       });
-	}
-}
-
+	  }
+  }
 }
 
 // Generate repos html as string
@@ -38,7 +28,7 @@ function myRepos(reposArray) {
     var result = '';
     reposArray.forEach(function(repo) {
       result += '<div class="repoData">'+
-                  '<p>Link : <a herf= class="repoName" src="'+repo.name+'" alt="">'+
+                  '<p>Link : <a herf="https://www.github.com/'+repo.name+'" class="repoName" alt="'+repo.name+'" target="_blank">'+
                     '<p id="overView">'+repo.desc+'</p>'+
                     '<p id="repoLang">'+repo.lang+'</p>'+
               '</div>';
@@ -59,4 +49,3 @@ function apiReq(userName , callback) {
   xhr.open('POST',"/search");
   xhr.send(userName);
 }
-
