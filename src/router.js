1017ac0600163969173965b766682820f6892e88
samer elaila <samer-elaila@live.com>
2017-08-09 14:56:58 +0300

    refactor backend

--- src/router.js
+++ src/router.js
@@ -9,8 +9,30 @@ function router(req, res){
   else if(url == '/404'){
     handlers.handleNotFound(req, res);
   }
+  // else if(url.startsWith('/:')){
+  //   res.writeHead(200 , {'Content-Type': 'text/html'});
+  //   let username = url.split("/:")[1];
+  //   if(username.includes('/')){
+  //     res.end('<h1>Username Not Found</h1>');
+  //   }
+  //   else if()
+  //   else {
+  //     req.username = username;
+  //     // handlers.handleSearch(req, res);
+  //     res.end('<h1>Username ' + username + ' is found</h1>');
+  //   }
+  // }
   else{
-    handlers.handleGeneric(req, res);
+    console.log('1',req.url);
+    if (url.includes('.') || url === '/') {
+      console.log('2222',req.url);
+        handlers.handleGeneric(req, res);
+    }else{
+      req.username = req.url.slice(1);
+      handlers.handleSearch(req, res);
+    }
+    console.log(req.url);
+
   }
 }
 
